692d9a5b0b948f4c425f69fe9d677e73d48ab5ce
Jason Chen <chentsungheng@hotmail.com>
2024-11-08 21:30:56 +0800

    實作新增邏輯

--- SqliteDemo.Logic/CustomerLogic.cs
+++ SqliteDemo.Logic/CustomerLogic.cs
@@ -1,4 +1,5 @@
-﻿using SqliteDemo.Logic.Base;
+﻿using Microsoft.Data.Sqlite;
+using SqliteDemo.Logic.Base;
 using SqliteDemo.Model;
 using SqliteDemo.Repository;
 
@@ -7,6 +8,8 @@ namespace SqliteDemo.Logic
     public interface ICustomerLogic : IDataLogic
     {
         Task<IEnumerable<Customer>> GetCustomersAsync(string? CustomerID, string? CompanyName, string? Region, string? PostalCode, CancellationToken TaskCancellationToken = default);
+
+        Task<Customer> AddCustomerAsync(Customer NewCustomer, CancellationToken TaskCancellationToken = default);
     }
 
     internal class CustomerLogic : DataLogic, ICustomerLogic
@@ -22,6 +25,49 @@ namespace SqliteDemo.Logic
                 var context = CreateSqliteRepository<ICustomerRepository>();
 
                 return await context.GetCustomersAsync(CustomerID, CompanyName, Region, PostalCode, DefaultTimeout, TaskCancellationToken);
+            }
+            catch (Exception ex)
+            {
+                throw LogException(ex);
+            }
+        }
+
+        public async Task<Customer> AddCustomerAsync(Customer NewCustomer, CancellationToken TaskCancellationToken = default)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(NewCustomer.CustomerID))
+                {
+                    throw new ArgumentException($"{nameof(NewCustomer.CustomerID)} is null or empty.");
+                }
+                if (string.IsNullOrEmpty(NewCustomer.CompanyName))
+                {
+                    throw new ArgumentException($"{nameof(NewCustomer.CompanyName)} is null or empty.");
+                }
+
+                var context = CreateSqliteRepository<ICustomerRepository>();
+                var exists = await context.GetCustomersAsync(NewCustomer.CustomerID, null, null, null, DefaultTimeout, TaskCancellationToken);
+
+                // 相同ID已存在
+                if (exists.Any())
+                {
+                    throw new InvalidOperationException($"The {nameof(NewCustomer.CustomerID)} {NewCustomer.CustomerID} is exists.");
+                }
+
+                using var transaction = OpenTransactionScope(DefaultTimeout);
+                var rows = await context.AddCustomerAsync(NewCustomer, DefaultTimeout, TaskCancellationToken);
+
+                // 新增失敗
+                if (rows == 0)
+                {
+                    throw new SqliteException("Add customer is failed.", 1);
+                }
+
+                transaction.Complete();
+
+                exists = await context.GetCustomersAsync(NewCustomer.CustomerID, null, null, null, DefaultTimeout, TaskCancellationToken);
+
+                return exists.Single();
             }
             catch (Exception ex)
             {
--- SqliteDemo.Test/Logic/CustomerLogicTest.cs
+++ SqliteDemo.Test/Logic/CustomerLogicTest.cs
@@ -1,10 +1,11 @@
 ﻿using SqliteDemo.Logic;
+using SqliteDemo.Model;
 
 namespace SqliteDemo.Test.Logic
 {
     internal class CustomerLogicTest
     {
-        [Test, Ignore("避免存取DB")]
+        [Test(Description = "讀取customer"), Ignore("避免存取DB")]
         public void GetCustomersAsync_Success()
         {
             using var factory = new BusinessLogicFactory();
@@ -12,6 +13,22 @@ namespace SqliteDemo.Test.Logic
             var actual = logic.GetCustomersAsync(null, null, null, null).Result;
 
             Assert.That(actual.Count(), Is.GreaterThan(0));
+        }
+
+        [Test(Description = "新增customer"), Ignore("避免存取DB")]
+        public void AddCustomerAsync_Success()
+        {
+            var data = new Customer
+            {
+                CustomerID = "TEST12345",
+                CompanyName = "Test"
+            };
+
+            using var factory = new BusinessLogicFactory();
+            var logic = factory.GetLogic<ICustomerLogic>();
+            var actual = logic.AddCustomerAsync(data).Result;
+
+            Assert.That(actual.CustomerID, Is.EqualTo(data.CustomerID));
         }
     }
 }
