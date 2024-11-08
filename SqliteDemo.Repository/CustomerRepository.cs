d43148dc7c068628eec1c5eb38fccc94c2ae98ac
Jason Chen <chentsungheng@hotmail.com>
2024-11-08 15:48:32 +0800

    實作新增方法

--- SqliteDemo.Repository/CustomerRepository.cs
+++ SqliteDemo.Repository/CustomerRepository.cs
@@ -9,6 +9,8 @@ namespace SqliteDemo.Repository
     public interface ICustomerRepository : ISqliteRepository
     {
         Task<IEnumerable<Customer>> GetCustomersAsync(string? CustomerID, string? CompanyName, string? Region, string? PostalCode, int Timeout = 30, CancellationToken SqlCancellationToken = default);
+
+        Task<int> AddCustomerAsync(Customer Data, int Timeout = 30, CancellationToken SqlCancellationToken = default);
     }
 
     internal class CustomerRepository : SqliteRepository, ICustomerRepository
@@ -21,6 +23,13 @@ namespace SqliteDemo.Repository
         {
             var columns = ConvertToColumnString<Customer>();
             var command = new StringBuilder($"SELECT {columns} FROM Customers WHERE 1 = 1");
+            var parameters = new
+            {
+                CustomerID,
+                CompanyName,
+                Region,
+                PostalCode
+            };
 
             if (!string.IsNullOrEmpty(CustomerID))
             {
@@ -39,7 +48,29 @@ namespace SqliteDemo.Repository
                 command.Append($" AND {nameof(PostalCode)} = @{nameof(PostalCode)}");
             }
 
-            return await Connection.QueryAsync<Customer>(GetCommand(CommandType.Text, command.ToString(), new { CustomerID, CompanyName, Region, PostalCode }, null, Timeout, SqlCancellationToken));
+            return await Connection.QueryAsync<Customer>(GetCommand(CommandType.Text, command.ToString(), parameters, null, Timeout, SqlCancellationToken));
+        }
+
+        public async Task<int> AddCustomerAsync(Customer Data, int Timeout = 30, CancellationToken SqlCancellationToken = default)
+        {
+            var columns = ConvertToColumnString<Customer>();
+            var command = new StringBuilder($"INSERT INTO Customers ({columns}) VALUES (@{nameof(Data.CustomerID)}, @{nameof(Data.CompanyName)}, @{nameof(Data.ContactName)}, @{nameof(Data.ContactTitle)}, @{nameof(Data.Address)}, @{nameof(Data.City)}, @{nameof(Data.Region)}, @{nameof(Data.PostalCode)}, @{nameof(Data.Country)}, @{nameof(Data.Phone)}, @{nameof(Data.Fax)})");
+            var parameters = new
+            {
+                Data.CustomerID,
+                Data.CompanyName,
+                Data.ContactName,
+                Data.ContactTitle,
+                Data.Address,
+                Data.City,
+                Data.Region,
+                Data.PostalCode,
+                Data.Country,
+                Data.Phone,
+                Data.Fax
+            };
+
+            return await Connection.ExecuteAsync(GetCommand(CommandType.Text, command.ToString(), parameters, null, Timeout, SqlCancellationToken));
         }
     }
 }
--- SqliteDemo.Test/Repository/CustomerRepositoryTest.cs
+++ SqliteDemo.Test/Repository/CustomerRepositoryTest.cs
@@ -1,4 +1,5 @@
-﻿using SqliteDemo.Logic.Base;
+﻿using AutoFixture;
+using SqliteDemo.Logic.Base;
 using SqliteDemo.Model;
 using SqliteDemo.Repository;
 
@@ -7,11 +8,13 @@ namespace SqliteDemo.Test.Repository
     internal class CustomerRepositoryTest
     {
         private AppSettings _settings;
+        private Fixture _fixture;
 
         [OneTimeSetUp]
         public void Init()
         {
             _settings = BusinessLogic.GetAppSettings();
+            _fixture = new Fixture();
         }
 
         [Test(Description = "成功取得Customer資料列"), Ignore("避免存取DB")]
@@ -23,6 +26,18 @@ namespace SqliteDemo.Test.Repository
 
             Assert.That(actual, Is.Not.Null);
             Assert.That(actual.Count(), Is.GreaterThan(0));
+        }
+
+        [Test(Description = "成功新增資料"), Ignore("避免存取DB")]
+        public void AddCustomerAsync_Success()
+        {
+            var data = _fixture.Build<Customer>().With(c => c.CustomerID, "TEST123").Create();
+
+            using var factory = new RepositoryFactory(_settings);
+            var context = factory.GetSqliteRepository<ICustomerRepository>();
+            var actual = context.AddCustomerAsync(data).Result;
+
+            Assert.That(actual, Is.GreaterThan(0));
         }
     }
 }
